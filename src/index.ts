e1be2920545e845b67864d288e04569a81834642
Bamidele Ajewole <bamideledavid.femi@gmail.com>
2024-04-24 12:37:57 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from 'express';
+
+const port = Number(process.env.PORT) || 8080;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send('Hello Bamidele, we have successfully deployed to Google App Engine');
+
+  console.log('Serviced a request')
+});
+
+app.get("/ping", (req: Request, res: Response) => {
+  res.status(200).send({ success: true });
+});
+
+
+app.get("/toyin", (req: Request, res: Response) => {
+    res.status(200).send({ firstname : "Toyin", surname: "Ajewole" });
+  });
+
+  
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
