3a5eb651db0c04be00a420ef4d399735b661d860
Bamidele Ajewole <bamideledavid.femi@gmail.com>
2024-04-26 09:22:49 +0100

    testing env variables

--- src/constants/EnvVars.ts
+++ src/constants/EnvVars.ts
@@ -1,7 +1,17 @@
 import { JwtKey, RefreshToken } from "../pre-start";
 import * as dotenv from 'dotenv';
+import logger from "../utils/logger";
 
 dotenv.config(); 
+
+function checker(){
+    logger.info(`Check if the data from prestart is actually gotten `);
+    logger.info(`JwtKey: ${JwtKey}, RefreshToken: ${RefreshToken}`);
+}
+
+if (process.env.NODE_ENV === 'production') {
+    checker();
+}
 
 export default {
     Port : (process.env.PORT ?? 8080),
